d4b1b12aab0979d8544a9bf1ae503031c64eec8f
ShwarzeWolf <31793871+ShwarzeWolf@users.noreply.github.com>
2017-11-25 18:19:43 +0300

    Update wav_core.h
    exception system was added

--- wav_core.h
+++ wav_core.h
@@ -4,29 +4,25 @@
 
 #include "wav_header.h"
 
-
-// TODO: Use an exception system to control errors.
 // TODO: Make support for 8-bit PSM, not just 16-bit.
 // TODO: Write a function to change the tone of the voice. (Interestingly, it's not too difficult?)
-
 
 class wav_core{
 public:
 
-	// Prints header's data from 'header_ptr' address.
 	void getInformation() const;
 
-	wav_errors_e readHeader(const char *filename){
-		return header.readHeader(filename);
+	void readHeader(const char *filename){
+		header.readHeader(filename);
 	};
 
 	// Reads file 'filename' and puts PCM data (raw sound data) to 'channels_data'.
 	// Also checks header validity, returns 'WAV_OK' on success.
-	wav_errors_e readDate(const char* filename, std::vector<std::vector<short>>& channels_data);
+	void readDate(const char* filename, std::vector<std::vector<short>>& channels_data);
 
 	// Creates a new WAV file 'filename', using 'sample_rate' and PCM data from 'channels_data'.
 	// Returns 'WAV_OK' on success.
-	wav_errors_e createWav(const char* filename, int sample_rate, const std::vector<std::vector<short>>& channels_data);
+	void createWav(const char* filename, int sample_rate, const std::vector<std::vector<short>>& channels_data);
 
 	// ************************************************************************
 	// * Functions for working with sound PCM data - Digital Signal Processing
@@ -34,11 +30,11 @@ public:
 
 	// Makes mono PCM data from stereo 'source'.
 	// Returns 'WAV_OK' on success.
-	wav_errors_e makeMono(const std::vector<std::vector<short>>& source, std::vector<std::vector<short>>& dest_mono);
+	void makeMono(const std::vector<std::vector<short>>& source, std::vector<std::vector<short>>& dest_mono);
 
 	// Adds a reverberation (it's like an echo).
 	// Returns 'WAV_OK' on success.
-	wav_errors_e makeReverb(std::vector<std::vector<short>>& sounds, int sample_rate, double delay_seconds, float decay);
+	void makeReverb(std::vector<std::vector<short>>& sounds, int sample_rate, double delay_seconds, float decay);
 
 	unsigned long getSampleRate(){
 		return header.date.sampleRate;
